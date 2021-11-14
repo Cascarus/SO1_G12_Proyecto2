e97e10ff8dc1afe0e07b802126d3c5f3765956e3
helmut <efraalv@gmail.com>
2021-11-13 20:04:38 -0600

    PubSub

---
+++ queues/pub-sub/subscriber/types/types.go
@@ -0,0 +1,12 @@
+package types
+
+type Log struct {
+
+    Request_number string `json:"request_number"`
+	Game string `json:"game"`
+	Gamename string `json:"gamename"`
+	Winner string `json:"winner"`
+	Players string `json:"players"`
+	Worker string `json:"worker"`
+
+}
