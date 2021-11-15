9f94079d07259f6392f3a2aea18fea11cb91636a
Edi Tomas <tomas3320141@gmail.com>
2021-11-15 07:22:17 -0600

    Update File
    it added the endpoints the file .

--- squidgame/Backend/src/routes/Games.js
+++ squidgame/Backend/src/routes/Games.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
-import { Top3Juegos } from "../services/Games.js";
+import { MostrarDatos, Top3Juegos ,Ultimo10_Juegos,MostrarInsersion,AgruparJugadores} from "../services/Games.js";
+
 const router = Router();
 
-
+// ruta  Juegostop3
 router.get('/Top3Juegos', Top3Juegos)
-
-// ruta  /Juegostop3
-// ruta  /Logs   //todos los datos 
-// ruta  /DatosAlmacenados del jugadores agrupados por jugador mongodb 
+// comparar los tres workers
+router.get('/MostrarInsersion', MostrarInsersion)
 // ruta  /Ultimo 10 juegos 
-// ruta  /Comparaciones  de los worker 
-// ruta  
+router.get('/Ultimo10_Juegos', Ultimo10_Juegos)
+// ruta  /Logs   //todos los datos 
+router.get('/MostrarDatos', MostrarDatos)
+// ruta  /DatosAlmacenados del jugadores agrupados por jugador mongodb 
+router.get('/AgruparJugadores', AgruparJugadores)
+
+
 
 
 export default router
--- squidgame/Backend/src/services/Games.js
+++ squidgame/Backend/src/services/Games.js
@@ -1,6 +1,5 @@
 import Logs from '../models/logs.js'
-//{ $match: {  } },
-
+import players  from '../models/players.js'
 export const Top3Juegos = async (req, res) => {
 
     /*const datos = await Logs.aggregate().
@@ -14,5 +13,31 @@ export const Top3Juegos = async (req, res) => {
         limit(3)
 
 
+    res.status(200).send(datos);
+}
+
+
+export const MostrarInsersion = async (req, res) => {
+    const datos = await Logs.aggregate().
+    group({ _id: "$worker", Count: { $sum: 1 } })
+    res.status(200).send(datos);
+}
+
+export const Ultimo10_Juegos = async (req, res) => {
+    const datos = await Logs.find().
+    limit(10)
+    res.status(200).send(datos);
+}
+
+export const MostrarDatos = async (req, res) => {
+    const datos = await Logs.find()
+    res.status(200).send(datos);
+}
+
+export const AgruparJugadores = async (req, res) => {
+    const datos = await players.aggregate().
+    group({ _id: "$jugador", JuegosGanado: { $sum: 1 } }).    
+    sort({ JuegosGanado: -1 })
+        
     res.status(200).send(datos);
 }
